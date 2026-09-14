51a6dbf212e7c0da3d83768c4240a72c44924e6a
asmi046 <asmi046@gmail.com>
2026-09-14 18:18:23 +0300

    + сертификаты

--- config/certificates.php
+++ config/certificates.php
@@ -1,17 +1,22 @@
 <?php
 
 return [
-
     ['image' => '/img/cert/gr_6.webp', 'title' => 'Грамота'],
     ['image' => '/img/cert/gr_5.webp', 'title' => 'Грамота'],
     ['image' => '/img/cert/gr_4.webp', 'title' => 'благодарственное письмо'],
     ['image' => '/img/cert/gr_3.webp', 'title' => 'Благодарственное письмо'],
     ['image' => '/img/cert/gr_2.webp', 'title' => 'Сертификат'],
     ['image' => '/img/cert/gr_1.webp', 'title' => 'Сертификат'],
+    ['image' => '/img/cert/gr_7.webp', 'title' => 'Сертификат'],
+    ['image' => '/img/cert/gr_8.webp', 'title' => 'Сертификат'],
+    ['image' => '/img/cert/gr_9.webp', 'title' => 'Сертификат'],
     ['image' => '/img/cert/gr_6.webp', 'title' => 'Грамота'],
     ['image' => '/img/cert/gr_5.webp', 'title' => 'Грамота'],
     ['image' => '/img/cert/gr_4.webp', 'title' => 'благодарственное письмо'],
     ['image' => '/img/cert/gr_3.webp', 'title' => 'Благодарственное письмо'],
     ['image' => '/img/cert/gr_2.webp', 'title' => 'Сертификат'],
     ['image' => '/img/cert/gr_1.webp', 'title' => 'Сертификат'],
+    ['image' => '/img/cert/gr_7.webp', 'title' => 'Сертификат'],
+    ['image' => '/img/cert/gr_8.webp', 'title' => 'Сертификат'],
+    ['image' => '/img/cert/gr_9.webp', 'title' => 'Сертификат'],
 ];
